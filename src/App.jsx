7d39d35bd51967430efea512e46d9485b0a479bd
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-04 23:26:11 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,8 @@ function App() {
   return (
     <>
       <div>
-        <head>
-  <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Tailwind CDN Example</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
+  <script src="https://cdn.tailwindcss.com"/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -22,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className = "text-3xl font-bold underline text-blue-600">Vite + React</h1>
+      <h1 className = "text-3xl font-bold underline text-blue-600">Mhmdnsr</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 3)}>
           count is {count}
