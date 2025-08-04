d1d3015a624886ed0c7deb854bf8988085198b02
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-05 01:25:43 +0330

    Create Login.jsx

---
+++ src/Login.jsx
@@ -0,0 +1,14 @@
+import StylishInput from "./StylishInput"
+import CustomBtn from "./CustomBtn"
+function Login() {
+  return (
+    <div>
+     <StylishInput />
+      <StylishInput />
+      <CustomBtn />
+      
+    </div>
+    );
+}
+
+export default Home;
