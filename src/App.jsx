2e81d2b66ef691111dce2e9ab235df9e2ae534e9
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-05 00:15:30 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import './index.css'
 import './output.css'
-function Button() {
-  return (
-    <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Click  
-    </button>
-  );
-}
 function App() {
 
   return (
      <div>
   <h6 className = "text-3xl font-bold underline text-blue-600">Mhmdnsr</h6>
-     <Button />
+     <button className = "bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Click  
+    </button>
      </div>
   )
 }
