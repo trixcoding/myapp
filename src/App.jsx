656c5a47fe0e812a2e21462780c54d54fff76727
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-05 01:30:01 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import './index.css'
 import './output.css'
 import CustomBtn from './CustomBtn'
 import StylishInput from './StylishInput'
+import Login from './Login'
 const handleClick = () => {
     return ;
   };
@@ -13,6 +15,22 @@ function App() {
       <CustomBtn onClick={handleClick} variant="outline">Cancel</CustomBtn>
       <StylishInput />
          <StylishInput />
+
+         
+      {/* منو */}
+      <nav style={{ marginBottom: '1rem' }}>
+        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+        <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
+      </nav>
+
+      {/* مسیرها */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
+          } />
+      </Routes>
+ 
      </div>
   )
 }
