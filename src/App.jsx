166c7fcb95efb1b4be44a7a3233f61ddf55a48ce
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-04 23:40:42 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <script src="https://cdn.tailwindcss.com"/></script>
- <h6 className = "text-3xl font-bold underline text-blue-600">Mhmdnsr</h6>
+     <div>
+  <h6 className = "text-3xl font-bold underline text-blue-600">Mhmdnsr</h6>
       </div>
   )
 }
