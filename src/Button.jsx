283f4d8f53ed5467415868a2ec72ba1f58932ea9
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-05 00:01:47 +0330

    Button.jsx

---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+function MyButton() {
+  return (
+    <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      کلیک کن
+    </button>
+  );
+}
+
+export default MyButton;
