e0acb34c23b5343de4695b2fd68753f1bd81f169
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-05 00:13:54 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './index.css'
 import './output.css'
-import Button from './Button'
 function Button() {
   return (
     <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
