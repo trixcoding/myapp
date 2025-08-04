f97ca0f299487d54fc72d58bf917d4361bdd43e8
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-05 01:46:52 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
 import './index.css'
 import App from './App.jsx'
 import './output.css'
