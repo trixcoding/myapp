b04d51a6b3e20dc963ae899ea0ab93df82f7d509
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-04 23:39:08 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
     <div>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <script src="https://cdn.tailwindcss.com"/></script>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+ <h6 className = "text-3xl font-bold underline text-blue-600">Mhmdnsr</h6>
       </div>
-      <h1 className = "text-3xl font-bold underline text-blue-600">Mhmdnsr</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 6)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
