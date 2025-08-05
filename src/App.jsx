06459375bc2d35fd65df1c1e3fc18ae1f29778b6
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-05 15:05:40 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function Home() {
 function About() {
     fetch('http://202.133.88.146:5000/api')
   .then(res => res.json())
-  .then(data => let x = data);
+  .then(data => x = data);
   return <h2>{x}</h2>;
     
 }
