39de9ffae6b6c3253f6f3581ae58bbf3811bda42
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-06 23:16:19 +0330

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -24,7 +24,8 @@ export default function ProductList() {
     <div className="max-w-7xl mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {products.map(product => (
         <div key={product.id} className="border rounded-lg shadow hover:shadow-lg transition p-4 flex flex-col">
-        <img 
+    <Link to="/product/1">محصول</Link>
+          <img 
             src={product.image} 
             alt={product.name} 
             className="w-full h-48 object-cover rounded-md mb-4"
