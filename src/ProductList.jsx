0619015c57ec1d35b558c24fef8b4299cc7b6570
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-06 22:58:33 +0330

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -24,8 +24,7 @@ export default function ProductList() {
     <div className="max-w-7xl mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {products.map(product => (
         <div key={product.id} className="border rounded-lg shadow hover:shadow-lg transition p-4 flex flex-col">
-         <Link to={`/product/${product.id}`}>
-          <img 
+        <img 
             src={product.image} 
             alt={product.name} 
             className="w-full h-48 object-cover rounded-md mb-4"
@@ -33,9 +32,11 @@ export default function ProductList() {
           />
           <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
           <p className="text-gray-700 font-bold text-xl">{product.price.toLocaleString()} تومان</p>
-          <button className="mt-auto bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition mt-4">
+         <Link to={`/product/${product.id}`}>
+            <button className="mt-auto bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition mt-4">
             خرید
           </button>
+           </Link>
         </div>
       ))}
     </div>
