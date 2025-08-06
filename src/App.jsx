2c159df2d982c53316163e5f8e8c9b457eb0b15d
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-06 22:01:27 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import SignUp from './SignUp';
 import Login from './Login';
 import Profile from './Profile';
 import Random from './Random';
+import ProductList from './ProductList';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<ProductList />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/random" element={<Random />} />
+        <Route path="/login" element={<login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
 
