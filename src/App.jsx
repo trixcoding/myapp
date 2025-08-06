a8fd31804b91335cdffd3c52834d680f69a0b67b
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-06 16:51:48 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import SignUp from "./SignUp"
+import Login from "./Login"
 function App() {
   return (
     <div>
+      <p>SignUp :</p>
     <SignUp />
+      <p>Signin</p>
+      <Login />
     </div>
 
     );
