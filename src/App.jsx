ecf8da57a52bae0e5b844285d9782f1b6043e465
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-06 17:51:22 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from './SignUp';
 import Login from './Login';
 import Profile from './Profile';
-import ShopProducts from './ShopProducts'; // اگه داری
-
+import Random from './Random';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-     
+        <Route path="/random" element={Random />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
