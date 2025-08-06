f8d30b803b7259e899208b70e4c8dc65658cec42
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-06 17:17:18 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import SignUp from "./SignUp"
-import Login from "./Login"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './SignUp';
+import Login from './Login';
+import Profile from './Profile';
+import ProductList from './ProductList'; // اگه داری
+
 function App() {
   return (
-    <div>
-      <p>SignUp :</p>
-    <SignUp />
-      <p>Signin</p>
-      <Login />
-    </div>
-
-    );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
