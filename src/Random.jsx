5675c721b5322c196fd84b7d9ceb9938f585017f
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-06 20:37:00 +0330

    Update Random.jsx

--- src/Random.jsx
+++ src/Random.jsx
@@ -4,7 +4,7 @@ import { QRCode } from 'qrcode.react';
 export default function Random() {
   return (
     <div className="p-10 text-center">
-      <h1 className="text-2xl font-bold mb-4">QR نمایش تست</h1>
+      <h1 className="text-2xl font-bold mb-4"> Test </h1>
       <QRCode value="hello world" />
     </div>
   );
